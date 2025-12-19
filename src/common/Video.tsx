d6cfa4953d31f61d8ced1c210f4a2272fc9d8a34
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-12-19 12:21:59 +0100

    fix: types in video

--- src/common/Video.tsx
+++ src/common/Video.tsx
@@ -7,10 +7,11 @@ import {
   MediaMuteButton,
   MediaPlaybackRateButton,
 } from "media-chrome/react";
+import type { MediaController as MediaControllerElement } from "media-chrome";
 import { useRef, useState } from "react";
 
 const SimplePlayer = () => {
-  const mediaControllerRef = useRef<HTMLMediaElement>(null);
+  const mediaControllerRef = useRef<MediaControllerElement>(null);
 
   const movementTimestamps = [0, 2.4, 4, 6.5, 9, 12, 15]; // cambiar por el json
   const [currentMovement, setCurrentMovement] = useState(0);
@@ -39,7 +40,9 @@ const SimplePlayer = () => {
 
   return (
     <>
-      <MediaController ref={mediaControllerRef}>
+      <MediaController
+        ref={mediaControllerRef as unknown as React.Ref<MediaControllerElement>}
+      >
         <video slot="media" src="/videos/won-hyo.mp4" preload="auto">
           <track default kind="chapters" src="/chapters/won-hyo.vtt" />
         </video>
